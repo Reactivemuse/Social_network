6bb54aa2259d8454b77e101e4454cc192d6de89a
Mitya <dim.maluckow2017@yandex.ru>
2024-10-29 23:26:20 +0300

    started create Messages component

--- src/Main/Messages/Messages.jsx
+++ src/Main/Messages/Messages.jsx
@@ -2,8 +2,21 @@ import s from "./Messages.module.css"
 
 const Messages = () => {
   return (
-    <div className={s.item}>
-      Messages
+    <div className={s.items}>
+      <div className={s.people}>
+        <div className={s.people__item + ' ' + s.active}>IGOR</div>
+        <div className={s.people__item}>DIMA</div>
+        <div className={s.people__item}>ARTEM</div>
+        <div className={s.people__item}>Kashirin</div>
+        <div className={s.people__item}>Antipov</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.messages__item}>Винни чемпион</div>
+        <div className={s.messages__item}>Привет</div>
+        <div className={s.messages__item}>Винни чемпион</div>
+        <div className={s.messages__item}>Нельзя писать state</div>
+        <div className={s.messages__item}>Исправь блок схему</div>
+      </div>
     </div>
   )
 }
