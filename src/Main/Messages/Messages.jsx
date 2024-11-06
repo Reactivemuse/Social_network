6c046f43d3a044364735022460db93671079b2e7
Mitya <dim.maluckow2017@yandex.ru>
2024-11-06 17:28:57 +0300

    added click event in messages component

--- src/Main/Messages/Messages.jsx
+++ src/Main/Messages/Messages.jsx
@@ -1,12 +1,22 @@
 import s from "./Messages.module.css"
 import People from "./People/People"
 import TextMessages from "./TextMessages/TextMessages"
+import React from "react"
 const Messages = (props) => {
-  
+  let message_item = React.createRef()
+
+  let message_function = () => {
+    let text = message_item.current.value
+    alert(text)
+  }
   return (
-    <div className={s.items}>
-      <People dialogsData = {props.dialogsData}/>
-      <TextMessages messagesData = {props.messagesData}/>
+    <div className={s.message_container}>
+      <div className={s.items}>
+        <People dialogsData = {props.dialogsData}/>
+        <TextMessages messagesData = {props.messagesData}/>
+      </div>
+      <textarea ref = {message_item}></textarea>
+      <button onClick={ message_function}>Добавить</button>
     </div>
   )
 }
