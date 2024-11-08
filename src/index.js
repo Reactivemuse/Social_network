a95fbb342e0b68157307937cfe47441edbe3fa08
Mitya <dim.maluckow2017@yandex.ru>
2024-11-08 23:44:44 +0300

    created Redux object store in state used OOP

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 
-import state from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { addpost, createtext, createtitle, subscribe } from './redux/state'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let renderElement = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} addpost={addpost} createtext={createtext} createtitle = {createtitle}/>
+      <App state={store.getState()} addpost={store.addpost.bind(store)} createtext={store.createtext.bind(store)} createtitle={store.createtitle.bind(store)}/>
     </React.StrictMode>
   )
 }
 
-renderElement(state)
+renderElement(store.getState())
 
-subscribe(renderElement)
+store.subscribe(renderElement)
 
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,80 +1,79 @@
 
-let renderElement = () => {
-  console.log("GOOD")
-}
+let store = {
+  _renderElement() {
+    console.log("GOOD")
+  },
+  _state: {
+    messagesPage: {
+      messagesData: [
+        { text: "Винни чемпион" },
+        { text: "Привет" },
+        { text: "Винни чемпион" },
+        { text: "Нельзя писать state" },
+        { text: "Исправь блок схему" },
+      ],
+      dialogsData: [
+        { id: 1, name: "IGOR" },
+        { id: 2, name: "DIMA" },
+        { id: 3, name: "ARTEM" },
+        { id: 4, name: "KASHIRIN" },
+        { id: 5, name: "ANTIPOV" },
+      ]
+    },
 
-let state = {
-  messagesPage: {
-    messagesData: [
-      { text: "Винни чемпион" },
-      { text: "Привет" },
-      { text: "Винни чемпион" },
-      { text: "Нельзя писать state" },
-      { text: "Исправь блок схему" },
-    ],
-
-    dialogsData: [
-      { id: 1, name: "IGOR" },
-      { id: 2, name: "DIMA" },
-      { id: 3, name: "ARTEM" },
-      { id: 4, name: "KASHIRIN" },
-      { id: 5, name: "ANTIPOV" },
-    ]
+    profilePage: {
+      postData: [
+        {
+          title: "History", text: "Напиши просто текст. Это простое задание, которое не требует особых усилий или специальных знаний. Просто открой свой блокнот или любой другой текстовый редактор и начни писать.", likes: "1"
+        },
+        {
+          title: "Задание", text: "Таким образом, ты сможешь развить свою письменную речь и станешь более уверенным в своих способностях.", likes: "5"
+        },
+        {
+          title: "Удачи!", text: "Главное - просто писать и выражать свои мысли.", likes: "9"
+        },
+        {
+          title: "Cтараемся!", text: "Пожалуйста, уделите немного времени, чтобы оценить результат.", likes: "100"
+        },
+        {
+          title: "Defense", text: "It seems like you may have intended to write 'defense' or 'defeats,' but since the topic is ambiguous, I'll provide a brief overview of both concepts for a better understanding.", likes: "1000"
+        },
+        {
+          title: "History", text: "Напиши просто текст. Это простое задание, которое не требует особых усилий или специальных знаний. Просто открой свой блокнот или любой другой текстовый редактор и начни писать.", likes: "1"
+        }
+      ],
+      postTitle: "",
+      postText: ""
+    }
+  },
+  getState(){
+    return this._state
+  },
+  addpost(){
+    let newElement = {
+      title: this._state.profilePage.postTitle,
+      text: this._state.profilePage.postText,
+      likes: 0
+    }
+    this._state.profilePage.postData.push(newElement);
+    this._state.profilePage.postTitle = ''
+    this._state.profilePage.postText = ''
+    this._renderElement(this._state)
+  },
+  createtitle(text){
+    this._state.profilePage.postTitle = text;
+    this._renderElement(this._state)
   },
 
-  profilePage: {
-    postData: [
-      {
-        title: "History", text: "Напиши просто текст. Это простое задание, которое не требует особых усилий или специальных знаний. Просто открой свой блокнот или любой другой текстовый редактор и начни писать.", likes: "1"
-      },
-      {
-        title: "Задание", text: "Таким образом, ты сможешь развить свою письменную речь и станешь более уверенным в своих способностях.", likes: "5"
-      },
-      {
-        title: "Удачи!", text: "Главное - просто писать и выражать свои мысли.", likes: "9"
-      },
-      {
-        title: "Cтараемся!", text: "Пожалуйста, уделите немного времени, чтобы оценить результат.", likes: "100"
-      },
-      {
-        title: "Defense", text: "It seems like you may have intended to write 'defense' or 'defeats,' but since the topic is ambiguous, I'll provide a brief overview of both concepts for a better understanding.", likes: "1000"
-      },
-      {
-        title: "History", text: "Напиши просто текст. Это простое задание, которое не требует особых усилий или специальных знаний. Просто открой свой блокнот или любой другой текстовый редактор и начни писать.", likes: "1"
-      }
-    ],
-    postTitle: "",
-    postText: ""
+  createtext(text){
+    this._state.profilePage.postText = text;
+    this._renderElement(this._state)
+  },
+
+  subscribe(observer){
+    this._renderElement = observer
   }
 
 }
 
-export const addpost = () => {
-  let newElement = {
-    title: state.profilePage.postTitle,
-    text: state.profilePage.postText,
-    likes: 0
-  }
-  state.profilePage.postData.push(newElement);
-  state.profilePage.postTitle = ''
-  state.profilePage.postText = ''
-  renderElement(state)
-}
-
-export const createtitle = (text) => {
-  state.profilePage.postTitle = text;
-  renderElement(state)
-
-}
-
-export const createtext = (text) => {
-  state.profilePage.postText = text;
-  renderElement(state)
-  
-}
-
-export const subscribe = (observer) => {
-  renderElement = observer
-}
-
-export default state
+export default store
