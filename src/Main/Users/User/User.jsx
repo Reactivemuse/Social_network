ee8fce2aed53b45d28b21bbd1166e405d1dfbc63
Mitya <dim.maluckow2017@yandex.ru>
2024-11-25 09:12:29 +0300

    fixed subscribe for user

--- src/Main/Users/User/User.jsx
+++ src/Main/Users/User/User.jsx
@@ -19,22 +19,6 @@ const User = (props) => {
         </NavLink>
         {props.followed === true ? 
         <button onClick={ () => {
-          axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {}, {
-            withCredentials: true,
-            headers: {
-              "API-KEY": "7f2efdc0-fd2c-4e71-aa63-a78d164bc576"
-            }
-          }).then(
-            responce => {
-              if (responce.data.resultCode === 0){
-                props.addFollow(props.id)
-              }
-            }
-          )
-      }
-
-        } className={s.button}>unfollow</button> : 
-        <button onClick={ () => {
           axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {
             withCredentials: true,
             headers: {
@@ -42,8 +26,24 @@ const User = (props) => {
             }
           }).then(
             responce => {
-              if (responce.data.resultCode === 0) {
+              if (responce.data.resultCode == 0){
                 props.addUnfollow(props.id)
+              }
+            }
+          )
+      }
+
+        } className={s.button}>unfollow</button> : 
+        <button onClick={ () => {
+          axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {}, {
+            withCredentials: true,
+            headers: {
+              "API-KEY": "7f2efdc0-fd2c-4e71-aa63-a78d164bc576"
+            }
+          }).then(
+            responce => {
+              if (responce.data.resultCode == 0) {
+                props.addFollow(props.id)
               }
             }
           )
